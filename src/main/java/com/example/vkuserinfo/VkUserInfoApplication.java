5ceaa11058fa36606a0ba2406aee69351bb20594
Роман <asoil2256@gmail.com>
2025-08-14 22:56:03 +0400

    without cache and tests

--- src/main/java/com/example/vkuserinfo/VkUserInfoApplication.java
+++ src/main/java/com/example/vkuserinfo/VkUserInfoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VUserInfoApplication {
+public class VkUserInfoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VUserInfoApplication.class, args);
+		SpringApplication.run(VkUserInfoApplication.class, args);
 	}
 
 }
